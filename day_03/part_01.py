f59e233aa451e7a9eaec85f3885ce39d6ee12222
Kay Lack <kay@kaylack.net>
2023-12-03 11:39:43 +0000

    Implement day 3

---
+++ day_03/part_01.py
@@ -0,0 +1,113 @@
+
+from dataclasses import dataclass, field
+import re
+
+from utility.main import check, every_line, hrange, pret, show
+
+
+@dataclass(frozen=True)
+class Coord:
+    x: int
+    y: int
+
+    def adjacents(self):
+        return {
+            Coord(self.x - 1, self.y - 1),
+            Coord(self.x, self.y - 1),
+            Coord(self.x + 1, self.y - 1),
+            Coord(self.x + 1, self.y),
+            Coord(self.x + 1, self.y + 1),
+            Coord(self.x, self.y + 1),
+            Coord(self.x - 1, self.y + 1),
+            Coord(self.x - 1, self.y),
+        }
+
+check(Coord(1, 1).adjacents(), {Coord(x=0, y=1), Coord(x=1, y=2), Coord(x=2, y=1), Coord(x=0, y=0), Coord(x=2, y=0), Coord(x=0, y=2), Coord(x=2, y=2), Coord(x=1, y=0)})
+
+@dataclass(frozen=True)
+class GridNumber:
+    coord: Coord
+    width: int
+    number: int
+
+    def get_cells_covering(self):
+        return {
+            Coord(x, self.coord.y)
+            for x in
+            hrange(self.coord.x, self.coord.x + self.width - 1, self.width)
+        }
+
+my_number = GridNumber(Coord(1, 1), 3, 123)
+check(my_number.get_cells_covering(), {Coord(1, 1), Coord(2, 1), Coord(3, 1)})
+
+@dataclass
+class GridSymbol:
+    coord: Coord
+    symbol: str
+
+@dataclass
+class State:
+    grid_numbers: list[GridNumber] = field(default_factory=list)
+    grid_symbols: list[GridSymbol] = field(default_factory=list)
+    valid_part_numbers: set[GridNumber] = field(default_factory=set)
+    cells_to_grid_numbers: dict[Coord, GridNumber] = field(default_factory=dict)
+
+    def viz(self):
+        show("", True)
+        grid = []
+        for y in range(0, 10):
+            row = []
+            for x in range(0, 10):
+                if Coord(x, y) in self.cells_to_grid_numbers:
+                    row.append("1")
+                elif Coord(x, y) in {s.coord for s in self.grid_symbols}:
+                    row.append("*")
+                else:
+                    row.append(".")
+            grid.append("".join(row))
+        show(", ".join(str(g.number) for g in self.valid_part_numbers))
+        show("\n".join(grid))
+
+    def get_valid_part_numbers(self):
+        for symbol in self.grid_symbols:
+            for coord in symbol.coord.adjacents():
+                if coord in self.cells_to_grid_numbers:
+                    self.valid_part_numbers.add(self.cells_to_grid_numbers[coord])
+
+    def sum_part_numbers(self):
+        return sum(
+            grid_number.number for grid_number in self.valid_part_numbers
+        )
+
+def parse_line(state: State, line, y):
+    number_matches = re.finditer(r"\d+", line)
+    grid_numbers = list(GridNumber(
+            Coord(match.start(), y), len(match.group(0)), int(match.group(0))
+        )
+        for match in number_matches
+    )
+    state.grid_numbers = state.grid_numbers + grid_numbers
+    for grid_number in grid_numbers:
+        for coord in grid_number.get_cells_covering():
+            state.cells_to_grid_numbers[coord] = grid_number
+    symbol_matches = re.finditer(r"[^.\d]", line)
+    state.grid_symbols = state.grid_symbols + list(
+        GridSymbol(Coord(match.start(), y), match.group(0))
+        for match in symbol_matches
+    )
+    return state
+
+my_state = State()
+parse_line(my_state, ".....+.58.", 5)
+check(my_state.grid_numbers, [GridNumber(Coord(7, 5), 2, 58)])
+check(my_state.grid_symbols, [GridSymbol(Coord(5, 5), "+")])
+
+def calculate(filename):
+    state = State()
+    state = every_line(state, filename, [parse_line])
+    state.get_valid_part_numbers()
+    state.viz()
+    return state.sum_part_numbers()
+
+# pret("Result:", calculate("trial.txt"))
+pret("Result:", calculate("input.txt"))
--- utility/main.py
+++ utility/main.py
@@ -2,7 +2,7 @@ import sys
 from dataclasses import dataclass
 from typing import Any, Callable, Collection, Sequence, Tuple, TypeVar
 from collections.abc import Iterable
-from PIL import Image, ImageDraw
+# from PIL import Image, ImageDraw
 
 # == Check ==
 # To replace assert and show nicer errors
@@ -113,6 +113,24 @@ def show(string: str, clear = None):
         print("\033c", file=sys.stderr)
     print(string, file=sys.stderr)
 
+# == adjacents ==
+# To calculate adjacent cells
+def adj(x, y, factory=None):
+    coords = {
+        (x - 1, y - 1),
+        (x, y - 1),
+        (x + 1, y - 1),
+        (x + 1, y),
+        (x + 1, y + 1),
+        (x, y + 1),
+        (x - 1, y + 1),
+        (x - 1, y),
+    }
+    if factory is not None:
+        return {factory(t[0], t[1]) for t in coords}
+    return coords
+
+
 # == Imager ==
 
 COLORS = {
@@ -164,72 +182,72 @@ class C3:
         else:
             raise Exception(f"Invalid faces {self.faces}")
 
-import matplotlib.pyplot as plt
-import numpy as np
+# import matplotlib.pyplot as plt
+# import numpy as np
 
-class CubeChart:
-    def __init__(self):
-        self.ax = plt.figure().add_subplot(projection='3d')
-        self.drawn = False
-        self.frame = 0
+# class CubeChart:
+#     def __init__(self):
+#         self.ax = plt.figure().add_subplot(projection='3d')
+#         self.drawn = False
+#         self.frame = 0
 
-    def __enter__(self):
-        plt.ion()
-        plt.show()
-        return self
+#     def __enter__(self):
+#         plt.ion()
+#         plt.show()
+#         return self
 
-    def redraw(self, items: list[C3], pause=0.001, recolor=False, filename=None):
-        if self.drawn is False or recolor is False:
-            self.items_length = len(items)
-            self.ax.clear()
-            self.ax.xaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f"{x/2:.0f}"))
-            self.ax.yaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f"{x/2:.0f}"))
-            self.ax.zaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f"{x/2:.0f}"))
-            self.ax.set_aspect('equal', 'box')
-            self.ax.set_box_aspect((1, 1, 1))
-            maxes = [max([item.coord[i] for item in items]) + 1 for i in range(3)]
-            overall_max = max(maxes)
-            self.ax.set_xlim(0, overall_max * 2 + 1)
-            self.ax.set_ylim(0, overall_max * 2 + 1)
-            self.ax.set_zlim(0, overall_max * 2 + 1)
-            data = np.zeros(tuple(maxes))
-            for item in items:
-                data[*item.coord] = True
-            data = self.explode(data)
-            x, y, z = self.expand_coordinates(np.indices(np.array(data.shape) + 1))
-            self.drawn = self.ax.voxels(x, y, z, data, facecolors="magenta", edgecolor="black", linewidth=0.1)
-        for item in items:
-            coord = (item.coord[0] * 2, item.coord[1] * 2, item.coord[2] * 2)
-            if coord in self.drawn:
-                self.drawn[coord].set_facecolor(item.colors())
-        if filename:
-            plt.savefig(filename.replace("NNN", f"{self.frame:03d}"))
-            self.frame += 1
-        else:
-            plt.draw()
-            plt.pause(pause)
+#     def redraw(self, items: list[C3], pause=0.001, recolor=False, filename=None):
+#         if self.drawn is False or recolor is False:
+#             self.items_length = len(items)
+#             self.ax.clear()
+#             self.ax.xaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f"{x/2:.0f}"))
+#             self.ax.yaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f"{x/2:.0f}"))
+#             self.ax.zaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f"{x/2:.0f}"))
+#             self.ax.set_aspect('equal', 'box')
+#             self.ax.set_box_aspect((1, 1, 1))
+#             maxes = [max([item.coord[i] for item in items]) + 1 for i in range(3)]
+#             overall_max = max(maxes)
+#             self.ax.set_xlim(0, overall_max * 2 + 1)
+#             self.ax.set_ylim(0, overall_max * 2 + 1)
+#             self.ax.set_zlim(0, overall_max * 2 + 1)
+#             data = np.zeros(tuple(maxes))
+#             for item in items:
+#                 data[*item.coord] = True
+#             data = self.explode(data)
+#             x, y, z = self.expand_coordinates(np.indices(np.array(data.shape) + 1))
+#             self.drawn = self.ax.voxels(x, y, z, data, facecolors="magenta", edgecolor="black", linewidth=0.1)
+#         for item in items:
+#             coord = (item.coord[0] * 2, item.coord[1] * 2, item.coord[2] * 2)
+#             if coord in self.drawn:
+#                 self.drawn[coord].set_facecolor(item.colors())
+#         if filename:
+#             plt.savefig(filename.replace("NNN", f"{self.frame:03d}"))
+#             self.frame += 1
+#         else:
+#             plt.draw()
+#             plt.pause(pause)
 
-    def explode(self, data):
-        size = np.array(data.shape)*2
-        data_e = np.zeros(size - 1, dtype=data.dtype)
-        data_e[::2, ::2, ::2] = data
-        return data_e
+#     def explode(self, data):
+#         size = np.array(data.shape)*2
+#         data_e = np.zeros(size - 1, dtype=data.dtype)
+#         data_e[::2, ::2, ::2] = data
+#         return data_e
 
-    def expand_coordinates(self, indices):
-        x, y, z = indices
-        x[1::2, :, :] += 1
-        y[:, 1::2, :] += 1
-        z[:, :, 1::2] += 1
-        return x, y, z
+#     def expand_coordinates(self, indices):
+#         x, y, z = indices
+#         x[1::2, :, :] += 1
+#         y[:, 1::2, :] += 1
+#         z[:, :, 1::2] += 1
+#         return x, y, z
 
-    def __exit__(self, exc_type, exc_value, traceback):
-        plt.ioff()
-        plt.close()
+#     def __exit__(self, exc_type, exc_value, traceback):
+#         plt.ioff()
+#         plt.close()
 
-# with CubeChart() as chart:
-#     from random import randint
-#     while True:
-#         random_tuple = (randint(0, 9), randint(0, 9), randint(0, 9))
-#         chart.redraw([C3(random_tuple, "RRGGBB"), C3((1, 1, 1), "RRGGBB")])
+# # with CubeChart() as chart:
+# #     from random import randint
+# #     while True:
+# #         random_tuple = (randint(0, 9), randint(0, 9), randint(0, 9))
+# #         chart.redraw([C3(random_tuple, "RRGGBB"), C3((1, 1, 1), "RRGGBB")])
 
 
